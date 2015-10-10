017ac4d58d86e2a1326076f96da9ea82d336f44a
Paweł Gajda <pawegio@gmail.com>
2015-10-10 23:38:50 +0200

    Optional query

--- app/src/main/java/com/backstopmedia/kotlin/KotlinApplication.kt
+++ app/src/main/java/com/backstopmedia/kotlin/KotlinApplication.kt
@@ -43,7 +43,7 @@ class KotlinApplication : Application() {
     }
 }
 
-fun SearchService.tweets(s: String, geocode: Geocode? = null, s1: String? = null, s2: String? = null, s3: String? = null,
-                         integer: Int? = null, s4: String? = null, aLong: Long? = null, aLong1: Long? = null,
-                         aBoolean: Boolean? = null, callback: Callback<Search>) =
+fun SearchService.tweets(s: String? = null, geocode: Geocode? = null, s1: String? = null, s2: String? = null,
+                         s3: String? = null,integer: Int? = null, s4: String? = null, aLong: Long? = null,
+                         aLong1: Long? = null, aBoolean: Boolean? = null, callback: Callback<Search>) =
         tweets(s, geocode, s1, s2, s3, integer, s4, aLong, aLong1, aBoolean, callback)
