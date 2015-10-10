cfe04c77edda771a9cd882656a37acf22bd488e4
Aaron Sarazan <aaron@levelmoney.com>
2015-10-10 12:42:19 -0700

    Create Kotlin Application class

---
+++ app/src/main/java/com/backstopmedia/kotlin/KotlinApplication.kt
@@ -0,0 +1,15 @@
+package com.backstopmedia.kotlin
+
+import android.app.Application
+
+
+/**
+ * Created by Aaron Sarazan on 10/10/15.
+ * Copyright(c) 2015 Level, Inc.
+ */
+class KotlinApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
