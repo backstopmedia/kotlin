f10a5d663793b22edb957da0d8cb53ee9e9017fb
Aaron Sarazan <aaron@levelmoney.com>
2015-10-10 12:52:25 -0700

    First draft of actual twitter integration.

--- app/src/main/java/com/backstopmedia/kotlin/KotlinApplication.kt
+++ app/src/main/java/com/backstopmedia/kotlin/KotlinApplication.kt
@@ -1,6 +1,12 @@
 package com.backstopmedia.kotlin
 
 import android.app.Application
+import android.util.Log
+import com.backstopmedia.kotlin.twitter.MyTwitter
+import com.twitter.sdk.android.Twitter
+import com.twitter.sdk.android.core.*
+import com.twitter.sdk.android.core.models.Search
+import io.fabric.sdk.android.Fabric
 
 
 /**
@@ -11,5 +17,26 @@ class KotlinApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        Fabric.with(this, Twitter(TwitterAuthConfig(MyTwitter.KEY, MyTwitter.SECRET)))
+
+        // Test run (or twitter the hard way!)
+        // We should be able to hack away a lot of this boilerplate.
+        Twitter.getInstance().core.logInGuest(object: Callback<AppSession>() {
+            override fun failure(p0: TwitterException?) {
+                throw UnsupportedOperationException()
+            }
+
+            override fun success(p0: Result<AppSession>?) {
+                Twitter.getApiClient().searchService.tweets("Kotlin", null, null, null, null, null, null, null, null, null, object: Callback<Search>() {
+                    override fun failure(p0: TwitterException?) {
+                        throw UnsupportedOperationException()
+                    }
+
+                    override fun success(p0: Result<Search>?) {
+                        Log.d("Kotlin", "Found ${p0?.data?.tweets?.size()?:0} tweets")
+                    }
+                })
+            }
+        })
     }
 }
