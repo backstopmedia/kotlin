6c7d6ff9d0c443351628a8f54b45c6ec54d6d25c
Aaron Sarazan <aaron@levelmoney.com>
2015-10-11 21:45:06 -0700

    break hello world into chapter3 module

--- chapter3/src/main/java/com/backstopmedia/kotlin/chapter3/JavaHelloActivity.java
+++ chapter3/src/main/java/com/backstopmedia/kotlin/chapter3/JavaHelloActivity.java
@@ -1,12 +1,10 @@
-package com.backstopmedia.kotlin.hello;
+package com.backstopmedia.kotlin.chapter3;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-
 import android.widget.EditText;
-import com.backstopmedia.kotlin.R;
 
 import static android.view.View.OnClickListener;
 
--- chapter3/src/main/java/com/backstopmedia/kotlin/chapter3/KotlinHelloActivity.kt
+++ chapter3/src/main/java/com/backstopmedia/kotlin/chapter3/KotlinHelloActivity.kt
@@ -1,10 +1,9 @@
-package com.backstopmedia.kotlin.hello
+package com.backstopmedia.kotlin.chapter3
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.Button
 import android.widget.EditText
-import com.backstopmedia.kotlin.R
 
 class KotlinHelloActivity : AppCompatActivity() {
 
