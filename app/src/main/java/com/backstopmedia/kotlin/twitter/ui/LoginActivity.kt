82bf65b0f144e1ae9a6bcc665dfe60bf4d5a1b89
Aaron Sarazan <aaron@levelmoney.com>
2015-10-15 22:48:31 -0700

    extensions

--- app/src/main/java/com/backstopmedia/kotlin/twitter/ui/LoginActivity.kt
+++ app/src/main/java/com/backstopmedia/kotlin/twitter/ui/LoginActivity.kt
@@ -15,10 +15,11 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-        login.callback = kallback {
-            toast("Logged in as ${it.data.userName}")
-            startActivity(LandingActivity::class.java)
-        }
+login.callback = kallback {
+    toast("Logged in as ${it.data.userName}")
+    startActivity(LandingActivity::class.java)
+    finish()
+}
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
