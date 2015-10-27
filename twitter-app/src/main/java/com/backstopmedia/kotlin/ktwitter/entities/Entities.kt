7a4234abcfaeb675802065bff24baf6071330255
Paweł Gajda <pawegio@gmail.com>
2015-10-27 10:53:24 +0100

    Entities/domain models in one .kt file

--- twitter-app/src/main/java/com/backstopmedia/kotlin/ktwitter/entities/Entities.kt
+++ twitter-app/src/main/java/com/backstopmedia/kotlin/ktwitter/entities/Entities.kt
@@ -1,6 +1,7 @@
 package com.backstopmedia.kotlin.ktwitter.entities
 
 import com.twitter.sdk.android.core.models.Tweet
+import com.twitter.sdk.android.core.models.User
 
 data class Image(val title: String, val retweetCount: Int, val mediaUrl: String) {
 
@@ -9,6 +10,15 @@ data class Image(val title: String, val retweetCount: Int, val mediaUrl: String)
             val title = tweet.text?.split("http")?.firstOrNull() ?: ""
             val url = tweet.entities?.media?.firstOrNull()?.mediaUrl?.concat(":large") ?: ""
             return Image(title, tweet.retweetCount, url)
+        }
+    }
+}
+
+data class Profile(val handle: String, val avatarUrl: String) {
+
+    companion object {
+        fun fromUser(user: User): Profile = with(user) {
+            Profile(handle = "@$screenName", avatarUrl = profileImageUrl.replace("_normal", ""))
         }
     }
 }
